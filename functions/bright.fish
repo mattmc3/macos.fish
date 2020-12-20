b6476093c85b6eb256cd08a45d1c5a2b7215990e
mattmc3 <mattmc3@gmail.com>
2020-12-20 17:03:27 -0500

    Change bright/dim

--- functions/bright.fish
+++ functions/bright.fish
@@ -1,4 +1,8 @@
-function bright -d 'Make the screen bright'
-    ensure-brew brightness
-    brightness .8
+function bright -a direction -d 'Make the screen bright'
+    switch "$direction"
+    case d down dim less
+        osascript -e 'tell application "System Events"' -e 'key code 145' -e ' end tell'
+    case u up more ""
+        osascript -e 'tell application "System Events"' -e 'key code 144' -e ' end tell'
+    end
 end
--- functions/dim.fish
+++ functions/dim.fish
@@ -1,6 +1,3 @@
 function dim -d 'Dim the screen'
-    if not type -q brightness
-        brew install brightness
-    end
-    brightness .2
+    osascript -e 'tell application "System Events"' -e 'key code 145' -e ' end tell'
 end
